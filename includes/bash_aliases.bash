1a79e2f8a4a60d38931e320e173db4a9f5a1de57
Reen Lokum <reen@alphacomm.nl>
2014-11-28 20:03:19 +0100

    Feature: gitclean command.
    Removes branches that are merged into master.

--- includes/bash_aliases.bash
+++ includes/bash_aliases.bash
@@ -2,3 +2,4 @@ alias sudo='sudo HOME="$HOME" SSH_CLIENT="$SSH_CLIENT" SSH_TTY="$SSH_TTY"'
 alias ls='ls --color=auto'
 alias svim='sudo vim'
 alias fuck='sudo $(history -p \!\!)' #https://twitter.com/liamosaur/status/506975850596536320/
+alias gitclean='git branch --merged master | grep -v "\smaster$" | grep -v "*" | xargs git branch -d'
