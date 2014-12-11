bda2a1b4ac5fe30677f18506f8099756b4e59c0b
Raymond Vermaas <raymond@alphacomm.nl>
2014-12-11 17:22:09 +0100

    Bugfix: Support slashes in branch names.

--- includes/bash_prompt.bash
+++ includes/bash_prompt.bash
@@ -24,10 +24,10 @@ prompt_venv_symbol="☁ "
 function ahead_behind() {
     curr_branch=$(git rev-parse --abbrev-ref HEAD);
     curr_remote=$(git config branch.$curr_branch.remote);
-    if [ "" ==  "$curr_remote" ]; then
+    curr_merge_branch=$(git config branch.$curr_branch.merge | cut -d / -f 3-);
+    if [ "" ==  "$curr_remote" ] || [ "" == "$curr_merge_branch" ]; then
         return;
     fi
-    curr_merge_branch=$(git config branch.$curr_branch.merge | cut -d / -f 3);
     git rev-list --left-right --count $curr_branch...$curr_remote/$curr_merge_branch | tr -s '\t' '|';
 }
 
