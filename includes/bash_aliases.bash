afd848fcc84e03f7b74616876443d9a200f77105
Reen Lokum <reen@alphacomm.nl>
2015-04-07 11:02:54 +0200

    Bugfix: fixcs alias typo.

--- includes/bash_aliases.bash
+++ includes/bash_aliases.bash
@@ -3,4 +3,5 @@ alias ls='ls --color=auto'
 alias svim='sudo vim'
 alias fuck='sudo $(history -p \!\!)' #https://twitter.com/liamosaur/status/506975850596536320/
 alias gitclean='git branch --merged master | grep -v "\smaster$" | grep -v "*" | xargs git branch -d'
-alias fixcs='php-cs-fix fix'
+alias fixcs='php-cs-fixer fix'
+alias latest='git tag | sort -V | tail -n 1'
