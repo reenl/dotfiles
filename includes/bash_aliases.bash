62c78bccec72cc1c83b8e6a61bf7f7d3b5fc9b42
Reen Lokum <reen@wodanbrothers.com>
2019-02-01 11:53:54 +0100

    Fix `myip` with tls

--- includes/bash_aliases.bash
+++ includes/bash_aliases.bash
@@ -6,7 +6,7 @@ alias fuck='sudo $(history -p \!\!)' #https://twitter.com/liamosaur/status/50697
 alias gitclean='git branch --merged master | grep -v "\smaster$" | grep -v "*" | xargs git branch -d'
 alias fixcs='php-cs-fixer fix'
 alias latest='git tag | sort -V | tail -n 1'
-alias myip='curl -sS ip.reenlokum.nl'
+alias myip='curl -sS https://ip.reenlokum.nl'
 alias service='sudo service'
 alias dclean='docker images -q --filter "dangling=true" | xargs docker rmi  > /dev/null 2>&1'
 alias punit='phpunit --no-coverage'
